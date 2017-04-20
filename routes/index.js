713e5e6209e2f71a0df68748fb8ac29d25709b25
b-tai <beverly_tai@brown.edu>
2017-04-20 19:47:01 -0400

    added very very basic RSS feed

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var anyDB = require('any-db');
 var conn = anyDB.createConnection('sqlite3://data/test.db');
 var unirest = require('unirest');
 const ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
+var parseString = require('xml2js').parseString;
 
 // ENTITIES:
 const DEPARTMENT_ENTITY = 'Brown_Department';
@@ -12,15 +13,19 @@ const SHUTTLE_STOP_ENTITY = 'ShuttleStop';
 const IS_DAYTIME_SHUTTLE = "isDayTimeShuttle";
 const EATERY_ENTITY = 'Eatery';
 const MEALTIME_ENTITY = 'MealTime';
+const FOODTYPE_ENTITY = 'FoodType';
 
 // INTENTS:
 const BROWN_YELLOWBOOK = 'Brown Yellowbook';
 const SHUTTLE = 'Shuttle';
 const SHUTTLE_FOLLOWUP = "Shuttle-followup";
-const DINING = "Dining";
+const DINING_MENU = "Dining";
+const DINING_HOURS = "DiningHours";
+const BROWN_EVENTS = 'BrownEvents';
 
 // CONTEXTS
 const SHUTTLE_CONTEXT = "shuttle-ctx";
+const DINING_CONTEXT = "dining-followup";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -60,25 +65,32 @@ const VEGETARIAN = "vegetarian_food";
 const PIZZA = "pizza";
 const SALAD = "salad";
 const OMELET = "omelet";
+const BREAKFAST = "breakfast";
+const SPECIAL = "special";
+const SOUP = "soup";
+const STIRFRY = "stirfry";
+const DELI = "deli";
 var course = new Map();
+// Ratty Constants
 course.set(RATTY + DESSERT, "12278"); 
 course.set(RATTY + COMFORT, "12176");
 course.set(RATTY + VEGETARIAN, "12177"); 
 course.set(RATTY + PIZZA, "12305");
 course.set(RATTY + SALAD, "12174");
 course.set(RATTY + OMELET, "13506");
+// Vdub Constants
+course.set(VDUB + BREAKFAST, "12368");
+course.set(VDUB + DESSERT, "12405");
+course.set(VDUB + COMFORT, "12419");
+course.set(VDUB + SPECIAL, "12369");
+course.set(VDUB + SOUP, "12404");
+course.set(VDUB + STIRFRY, "12420");
+course.set(VDUB + SALAD, "12402");
+course.set(VDUB + DELI, "12403");
 
-var rattyChangingDishes = [course.get(RATTY + DESSERT), course.get(RATTY + VEGETARIAN), course.get(RATTY + COMFORT)];
-
-// var promise = getDiningTime("Ratty", formatDate(new Date()), "Breakfast");
-// promise.then(function (message) {
-//   console.log(message);
-// });
-
-// var promise2 = getFoodItem(5767821);
-// promise2.then(function (message) {
-//   console.log(message);
-// });
+const defaultChangingDishes = new Map();
+defaultChangingDishes.set(VDUB, [course.get(VDUB + BREAKFAST), course.get(VDUB + DESSERT), course.get(VDUB + COMFORT), course.get(VDUB + SOUP), course.get(VDUB + STIRFRY)]);
+defaultChangingDishes.set(RATTY, [course.get(RATTY + DESSERT), course.get(RATTY + VEGETARIAN), course.get(RATTY + COMFORT)]);
 
 /*
   returns date in the format "YYYY-MM-DD"
@@ -118,11 +130,14 @@ function getMeal(menu, mealTime) {
 function getDiningMenu(diningHallName, date, mealType, courses=[]) {
   var includeAllCourses = courses.length == 0;
   var diningHallCode = diningHalls.get(diningHallName);
+  
   return new Promise(function(resolve, reject) { 
     unirest.get("http://legacy.cafebonappetit.com/api/2/menus?cafe="+ diningHallCode +"&date=" + date)
       .header("Accept", "application/json")
       .end(function (result) {
+
           var menu = getDiningHallInfo(result, diningHallCode);
+
           var item_lookup = result.body.items;
           var allMealItems = getMeal(menu, mealType);
           var stations = allMealItems["stations"];
@@ -146,20 +161,22 @@ function getDiningMenu(diningHallName, date, mealType, courses=[]) {
   });
 }
 
+// date should be formatted
 function getDiningTime(diningHallName, date, mealType) {
   var diningHallCode = diningHalls.get(diningHallName);
   return new Promise(function(resolve, reject) {
       unirest.get("http://legacy.cafebonappetit.com/api/2/menus?cafe="+ diningHallCode +"&date=" + date)
         .header("Accept", "application/json")
         .end(function (result) {
-            var eatery = getDiningHallInfo(result, diningHallCode);
-            var mealTypeInfo = getMeal(eatery, mealType);
-            resolve({start: mealTypeInfo.starttime, end: mealTypeInfo.endtime});
+          var eatery = getDiningHallInfo(result, diningHallCode);
+          var mealTypeInfo = getMeal(eatery, mealType);
+          resolve({start: mealTypeInfo.starttime, end: mealTypeInfo.endtime});
     });
   });
 }
 
 /*
+  Current Unused
   input: id of food item
   output: name of food associated with the id
 */
@@ -185,17 +202,50 @@ function getNumber(assistant) {
 
 function handleDining(assistant) {
   var eatery = assistant.getArgument(EATERY_ENTITY);
-  var meal_time = assistant.getArgument(MEALTIME_ENTITY)
-  var menu_items = getDiningMenu(eatery, formatDate(new Date()), meal_time, rattyChangingDishes);
+  var meal_time = assistant.getArgument(MEALTIME_ENTITY);
+  var food_types = assistant.getArgument(FOODTYPE_ENTITY);
+
+  var coursesToLookUp = [];
+  var message = "";
+  if (food_types.length == 0) {
+    coursesToLookUp = defaultChangingDishes.get(eatery);
+    message = "The rotating items on today's menu for " + meal_time + " at the " + eatery + " are: ";
+  } else {
+    for (var i = 0; i < food_types.length; i++) {
+      coursesToLookUp.push(course.get(eatery + food_types[i]));
+      message = message + food_types[i] + " ";
+      if (i == food_types.length - 2) {
+        message = message + " and ";
+      }
+    }
+    message = message + " options at the " + eatery + " for " + meal_time + " are: ";
+  }
+  
+  var menu_items = getDiningMenu(eatery, formatDate(new Date()), meal_time, coursesToLookUp);
   menu_items.then(function (items) {
-    var message = "The rotating items on today's menu at the " + eatery + " are: ";
     for (var i = 0; i < items.length; i++) {
       message = message + " " + items[i] + ",";
       if (i == items.length - 2) {
         message = message  + " and";
       }
     }
-    assistant.tell(message);
+
+    if (items.length == 0) {
+      assistant.tell("Sorry, I couldn't find any items.")
+    } else {
+      assistant.tell(message);
+    }
+  });
+}
+
+function handleDiningHours(assistant){
+  console.log("Handling Dining Hours");
+  var eatery = assistant.getArgument(EATERY_ENTITY);
+  var mealTime = assistant.getArgument(MEALTIME_ENTITY);
+
+  var hours_promise = getDiningTime(eatery, formatDate(new Date()), mealTime);
+  hours_promise.then(function (ret) {
+    assistant.tell(eatery + " " + mealTime + " hours are " + ret.start + " to " + ret.end + " today "); // to do: be able to check for other days (not just today)
   });
 }
 
@@ -211,26 +261,27 @@ function handleShuttle(assistant) {
             break;
         }
     }
+    console.log("has prior context: " + hasPriorContext);
     if (hasPriorContext) {
         var originalName = name;
         name = "%"+name.toLowerCase()+"%";
         var route_id = assistant.getArgument(IS_DAYTIME_SHUTTLE);
         route_id = route_id.includes("daytime")? dayNightShuttle.get("Daytime"):dayNightShuttle.get("Night");
+
         conn.query("SELECT * FROM shuttleStops WHERE " +
             "name like $1 and route_id= $2",
             [name, route_id], function(err, result){
-                var rowCount = result.rowCount;
-                if (rowCount == 0) {
+                if (err) {
+                  console.log(err);
+                    assistant.tell("Sorry, there was an error occurred when I tried to find the stop_id for" +
+                        "stop " + originalName);
+                } else if (result.rowCount == 0) {
                     assistant.tell("Sorry, but I don't know a shuttle stop called " + originalName);
                 } else if (!err) {
                         var stop_id = result.rows[0].stop_id;
                         if (stop_id !== undefined && stop_id !== "") {
                             requestArrivalEstimatesByStopId(originalName, stop_id, assistant);
                         }
-                } else {
-                    console.log(err);
-                    assistant.tell("Sorry, there was an error occurred when I tried to find the stop_id for" +
-                        "stop " + originalName);
                 }
             });
     } else {
@@ -244,10 +295,10 @@ function handleShuttle(assistant) {
             [name], function(err, result){
                 var rowCount = result.rowCount;
                 if (rowCount == 0) {
-                    assistant.tell("Sorry, but I don't know a shuttle stop called " + name);
+                    assistant.tell("Sorry, but I don't know a shuttle stop called " + originalName);
                 } else if (!err) {
                     if (rowCount > 1) {
-                        console.log("more than 1 rowCount");
+                        //console.log("more than 1 rowCount");
                         assistant.setContext(SHUTTLE_CONTEXT, 2, {shuttleName: originalName});
                         assistant.ask("Are you asking for daytime shuttle or evening time shuttle?");
                     } else {
@@ -286,13 +337,28 @@ function requestArrivalEstimatesByStopId(name, stop_id, assistant) {
                 arrival_time.setSeconds(0);
 
                 if (date_diff.getMinutes() < 1) {
-                  assistant.tell("The shuttle is arriving now to " + name );
+                  assistant.tell("The shuttle is arriving now at " + name);
                 } else {
                 //TODO: add time from now (ie: "will arrive in 5 minutes at 6:24pm")
                   assistant.tell("The next shuttle will arrive at " + name + " in " + date_diff.getMinutes() + " minutes " + " at " + arrival_time.toLocaleTimeString());
                 }
             }
         });
+}
+
+function handleBrownEvents(assistant) {
+  unirest.get("http://events.brown.edu:/cal/main/listEvents.do?b=de&skinName=rss-list&setappvar=summaryMode(details)&days=5&cat=CareerLAB")
+    .header("Accept", "application/xml")
+    .end(function (result) {
+      parseString(result.body, function (err, result) {
+        var list_events = result.rss.channel[0].item;
+        var message = "";
+        for (var i = 0; i < list_events.length; i++) {
+          message = message + " " + list_events[i].title; // TODO: if they want to look up more events.... look up details, etc.
+        }
+        assistant.tell(message);
+      });
+  });
 }
 // var yellowBookMap = new Map();
 // yellowBookMap.set("DPS", "401123456789");
@@ -305,7 +371,7 @@ router.post('/', function(req, res, next) {
   function responseHandler (assistant) {
 
     const intent = req.body.result.metadata.intentName;
-    
+    console.log(intent);
     switch (intent) {
      // case BROWN_YELLOWBOOK:
      //    getNumber(assistant);
@@ -315,11 +381,17 @@ router.post('/', function(req, res, next) {
         break;
       case SHUTTLE_FOLLOWUP:
         handleShuttle(assistant);
-      case DINING:
+        break;
+      case DINING_MENU:
         handleDining(assistant);
+        break;
+      case DINING_HOURS:
+        handleDiningHours(assistant);
+      case BROWN_EVENTS:
+        handleBrownEvents(assistant);
+        break;
     }
   }
-
   assistant.handleRequest(responseHandler);
   //res.send();
 
