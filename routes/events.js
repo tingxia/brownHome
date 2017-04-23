0189f381adda2dfe15d7085dfcb5b2c7980ab83c
b-tai <beverly_tai@brown.edu>
2017-04-23 16:08:50 -0400

    Created dining and events files to clean-up code

---
+++ routes/events.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Bev on 4/23/17.
+ */
+var unirest = require('unirest');
+var parseString = require('xml2js').parseString;
+
+const TIME_PERIOD_PARAMETER = 'TimePeriod'; // used in BrownEvents intent
+const EVENT_CATEGORY_ENTITY = 'EventCategory';
+
+module.exports = {
+    handleBrownEvents: function (assistant) {
+        var days = calcDays(assistant.getArgument(TIME_PERIOD_PARAMETER));
+        var eventCategory = assistant.getArgument(EVENT_CATEGORY_ENTITY);
+        if (eventCategory == null) {
+            eventCategory = "";
+        }
+        var message = eventCategory + " events in the next " + days + " days are: ";
+        unirest.get("http://events.brown.edu:/cal/main/listEvents.do?b=de&skinName=rss-list&setappvar=summaryMode(details)&days=" + days + "&cat=" + eventCategory)
+            .header("Accept", "application/xml")
+            .end(function (result) {
+                parseString(result.body, function (err, result) {
+                    var list_events = result.rss.channel[0].item;
+                    for (var i = 0; i < list_events.length; i++) {
+                        message = message + " " + list_events[i].title;
+                    }
+                    if (list_events.length == 0) {
+                        assistant.tell("There are no " + eventsCategory + " events in the next " + days + "days");
+                    } else {
+                        assistant.tell(message);
+                    }
+                });
+            });
+    }
+};
+
+//  input duration: {"amount": 10, "unit": min} or string: today, week, month
+function calcDays(duration) {
+    if (duration == "day" || duration == "today") {
+        return 1;
+    } else if (duration == "week"){
+        return 7;
+    } else if (duration == "month") {
+        return 31;
+    }
+
+    switch (duration["unit"]) {
+        case "day":
+            return duration.amount;
+        case "wk":
+            return duration.amount * 7;
+        case "month":
+            return duration.amount * 31;
+    }
+
+    return 2; // by default returns 2 days
+}
